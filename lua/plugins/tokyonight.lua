a7b141900e3195412c3578f952fd2282f4f52b61
Rodrigo Borlone Espoz <rborlone@ccs.cl>
2026-08-17 11:50:20 -0400

    Initial commit: LazyVim config

---
+++ lua/plugins/tokyonight.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    "folke/tokyonight.nvim",
+    opts = {
+      style = "night", -- "storm" | "moon" | "night" | "day"
+      transparent = false,
+      styles = {
+        comments = { italic = true },
+        keywords = { italic = false },
+      },
+    },
+  },
+}
